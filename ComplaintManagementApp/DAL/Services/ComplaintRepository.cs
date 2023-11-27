42786042d34e1688a50d047b12b88449403d40ef
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-27 12:43:28 +0530

    Add files via upload

---
+++ ComplaintManagementApp/DAL/Services/ComplaintRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using ComplaintManagementApp.DAL.Interfaces;
+
+namespace ComplaintManagementApp.DAL.Services
+{
+    public class ComplaintRepository : Interfaces.IComplaintRepository
+    {
+        private ComplaintDbContext _context;
+
+        public ComplaintRepository(ComplaintDbContext context)
+        {
+            _context = context;
+        }
+
+        public Model.ComplaintModel GetById(int id)
+        {
+            return _context.ComplaintModels.FirstOrDefault(t => t.ComplaintId == id);
+        }
+
+        public string GetAll()
+        {
+            string qry = "select* from ComplaintModels";
+            return qry;
+        }
+
+        public string Add()
+        {
+            string qry = "insert into ComplaintModels(Title, IsResolved, DueDate)" +
+                "values('";
+            return qry;
+        }
+
+        public string Update()
+        {
+            var query = "update ComplaintModels set Title='";
+            return query;
+        }
+
+        public string Delete()
+        {
+            var query = "delete from ComplaintModels where Id='";
+            return query;
+        }
+    }
+}
---
+++ ComplaintManagementApp/DAL/Services/ComplaintService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using ComplaintManagementApp.DAL.Interfaces;
+using ComplaintManagementApp.Model;
+
+namespace ComplaintManagementApp.DAL.Services
+{
+    public class ComplaintService : Interfaces.IComplaintService
+    {
+        private Interfaces.IComplaintRepository _repository;
+
+        public ComplaintService(Interfaces.IComplaintRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        public string GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public string Add()
+        {
+            return _repository.Add();
+        }
+
+        public string Update()
+        {
+            return _repository.Update();
+        }
+
+        public string Delete()
+        {
+            return _repository.Delete();
+        }
+    }
+}
